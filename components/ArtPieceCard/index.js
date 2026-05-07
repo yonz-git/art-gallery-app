5520866113576596a9fbbad7a86af1d1875a4a67
Simon Nancekievill <simon.nancekievill@gmail.com>
2026-05-07 17:43:05 +0200

    adding colors to detail page

--- components/ArtPieceCard/index.js
+++ components/ArtPieceCard/index.js
@@ -3,24 +3,15 @@ import Favourite from "@/assets/favourite.svg";
 import Link from "next/link";
 import { GetArtPiecesData } from "@/libs/artPieces";
 
-export default function ArtPieceCard() {
-  const { artPieces, error, isLoading } = GetArtPiecesData();
-
-  if (error) {
-    console.error("ERROR!", error.info);
-
-    return <div>failed to load</div>;
-  }
-
-  if (isLoading || !artPieces) return <div>loading...</div>;
-  console.dir(artPieces);
-
+export default function ArtPieceCard({ foundArtPiece }) {
+  const { slug, imageSource, name, artist } = foundArtPiece;
+  console.log(foundArtPiece);
   return (
     <>
       <span>
         <Link href="/art-pieces">Go Back to Overview</Link>
       </span>
-      {/* <section>
+      <section>
         <Link href={`/art-pieces/${slug}`}>
           {" "}
           <Image src={imageSource} alt={name} width="300" height="150" />
@@ -32,7 +23,7 @@ export default function ArtPieceCard() {
         <h2>
           „{name}“ by {artist}
         </h2>
-      </section> */}
+      </section>
     </>
   );
 }
