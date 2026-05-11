a5e6626772659b5478d546f267c59c7db8d01f82
Simon Nancekievill <simon.nancekievill@gmail.com>
2026-05-11 11:27:57 +0200

    success message is added

--- components/CommentsForm/index.js
+++ components/CommentsForm/index.js
@@ -1,6 +1,12 @@
 import CommentsList from "../CommentsList";
 import { uid } from "react-uid";
-import { Form, Label, InputField, SubmitButton } from "./CommentsForm.styled";
+import {
+  Form,
+  Label,
+  InputField,
+  SubmitButton,
+  SuccessMessage,
+} from "./CommentsForm.styled";
 import { useState } from "react";
 
 export default function CommentsForm({ artPieces, artPiece, setArtPieces }) {
@@ -27,6 +33,8 @@ export default function CommentsForm({ artPieces, artPiece, setArtPieces }) {
           : art
       )
     );
+    setShowSuccess(true);
+    setTimeout(() => setShowSuccess(false), 2000);
     event.target.reset();
     event.target.comment.focus();
   }
@@ -45,6 +53,11 @@ export default function CommentsForm({ artPieces, artPiece, setArtPieces }) {
         />
         <SubmitButton type="submit">Send</SubmitButton>
       </Form>
+      {showSuccess && (
+        <SuccessMessage aria-label="Success Message for adding a new Comment">
+          <p>✓ Comment successfully added!</p>
+        </SuccessMessage>
+      )}
       <CommentsList comments={artPiece.comments} />
     </>
   );
