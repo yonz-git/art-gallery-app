519d815eb6b0633db7c510a39d2404d60259ddbf
Marco <m.hebisch@outlook.com>
2026-05-11 10:59:48 +0200

    styled the like button

--- components/Spotlight/index.js
+++ components/Spotlight/index.js
@@ -3,28 +3,31 @@ import Image from "next/image";
 import FavoriteButton from "@/components/FavoriteButton";
 import { ToggleFavorite } from "@/libs/artPieces";
 import {} from "./Spotlight.styled";
+import { ImageWrap } from "../ArtPiecePreview/ArtPiecePreview.styled";
 
 export default function Spotlight({ artPieces, artPiece, setArtPieces }) {
   return (
     <>
-      <Link href="/art-pieces">
-        <Image
-          src={artPiece.imageSource}
-          alt={artPiece.name}
-          width="300"
-          height="150"
-          style={{
-            objectFit: "cover",
-          }}
+      <ImageWrap>
+        <Link href="/art-pieces">
+          <Image
+            src={artPiece.imageSource}
+            alt={artPiece.name}
+            width="300"
+            height="150"
+            style={{
+              objectFit: "cover",
+            }}
+          />
+        </Link>
+        <FavoriteButton
+          onToggleFavorite={() =>
+            setArtPieces(ToggleFavorite(artPieces, artPiece))
+          }
+          isFavorite={artPiece.isFavorite}
+          disabled
         />
-      </Link>
-      <FavoriteButton
-        onToggleFavorite={() =>
-          setArtPieces(ToggleFavorite(artPieces, artPiece))
-        }
-        isFavorite={artPiece.isFavorite}
-        disabled
-      />
+      </ImageWrap>
       <h2>{artPiece.artist}</h2>
     </>
   );
